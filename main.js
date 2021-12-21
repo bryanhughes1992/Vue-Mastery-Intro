5ba08d9c9867ae29abb78337ad085a930889c88d
Bryan Hughes <bhughes@evertz.com>
2021-12-21 17:32:41 -0500

    initial upload

--- main.js
+++ main.js
@@ -1,1 +1,50 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      cart: 0,
+      product: 'Socks',
+      brand: 'Vue Mastery',
+      description: "Vue Socks",
+      selectedVariant: 0,
+      url: 'https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3',
+      inventory: 100,
+      onSale: true,
+      details: [
+        '50% cotton',
+        '30% wool',
+        '20% polyester'
+      ],
+      variants: [
+        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
+      ],
+      sizes: [
+        'x-small',
+        'small',
+        'medium',
+        'large',
+        'x-large',
+        'xx-large'
+      ]
+    }
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1
+    },
+    updateVariant(index) {
+      this.selectedVariant = index
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product
+    },
+    image() {
+      return this.variants[this.selectedVariant].image
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity
+    }
+  }
+})
